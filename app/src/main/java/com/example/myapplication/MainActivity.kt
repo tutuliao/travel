d0de49153dfbd0068c4dc0b535e622c18507befd
Jacky <717990694@qq.com>
2023-12-27 20:33:59 +0800

    底部导航栏databinding主页完成绑定

--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -1,32 +1,31 @@
 package com.example.myapplication
-
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
-import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     /*因为android app有生命周期
     所以入口点是onCreate，而不是main函数*/
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-    }
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(
+            this, R.layout.activity_main
+        )
+        binding.bottomNavigationView.setOnNavigationItemReselectedListener { menuItem ->
+            when (menuItem.itemId) {
+                R.id.navigation_home -> {
+                    true
+                }
 
-    var sum = 0;
+                R.id.navigation_dashboard -> {
+                    true
+                }
 
-    fun increment(view: View){
-       sum++;
-       showScores(sum);
-    }
-
-    fun decrement(view: View){
-        sum--;
-        showScores(sum);
-    }
-
-    private fun showScores(sum: Int) {
-      val textView: TextView = findViewById(R.id.textView1);
-        textView.text = sum.toString();
+                R.id.navigation_notifications -> {
+                    true
+                }
+            }
+        }
     }
 }
