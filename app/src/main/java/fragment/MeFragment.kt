cfec8be29cf6f7da10579fedb2cb2fbef7024172
Jacky <717990694@qq.com>
2024-01-04 03:23:42 +0800

    修改了build.gradlew内的编译版本号33为34 添加了头像

--- app/src/main/java/fragment/MeFragment.kt
+++ app/src/main/java/fragment/MeFragment.kt
@@ -1,12 +1,17 @@
 package fragment
 import android.os.Bundle
 import android.view.View
+import android.widget.ImageView
+import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
+import com.example.myapplication.databinding.ActivityMainBinding
 
 class MeFragment : Fragment(R.layout.me_page){
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
