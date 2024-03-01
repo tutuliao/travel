31de43dfb1c308eee4f00a58687b84b1ffe25840
Jacky <717990694@qq.com>
2024-03-01 21:56:48 +0800

    主页item的文字大小间距以及点击收藏的按钮设置完成

---
+++ app/src/main/java/com/example/myapplication/CollectItemActivity.kt
@@ -0,0 +1,13 @@
+package com.example.myapplication
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+
+class CollectItemActivity : AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+    }
+
+
+}
