a48857841bcd476e9958855c8ca8f2f72c7d0034
Jacky <717990694@qq.com>
2024-03-22 04:38:58 +0800

    搜索接口结合binding没问题

--- app/src/main/java/fragment/SearchFragment.kt
+++ app/src/main/java/fragment/SearchFragment.kt
@@ -8,10 +8,26 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import com.example.myapplication.R
 import com.example.myapplication.databinding.SearchPageBinding
+import com.scwang.smart.refresh.layout.SmartRefreshLayout
+import http.RetrofitManager
+import io.reactivex.Observer
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.Disposable
+import io.reactivex.schedulers.Schedulers
+import model.ItemCollectedListManager
+import model.ItemCollectedResponse
+import okhttp3.ResponseBody
+import retrofit2.Call
+import retrofit2.Response
+import service.Toast
 
-class SearchFragment : Fragment(R.layout.search_page){
+class SearchFragment : Fragment(R.layout.search_page) {
 
-    private lateinit var binding : SearchPageBinding
+    private lateinit var binding: SearchPageBinding
+    private lateinit var smartRefreshLayout: SmartRefreshLayout
+    private var apiService = RetrofitManager.getInstance().provideApiService()
+    private var index = 0
+    private var inputText = ""
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
     }
@@ -28,11 +44,32 @@ class SearchFragment : Fragment(R.layout.search_page){
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        binding.searchButton.setOnClickListener{
-
-        }
         super.onViewCreated(view, savedInstanceState)
+        binding.searchButton.setOnClickListener{
+            inputText = binding.searchInput.text.toString()
+            apiService.getSearchList(inputText,0)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(object : Observer<Response<ResponseBody>> {
+                    override fun onSubscribe(d: Disposable) {}
+
+                    override fun onNext(response: Response<ResponseBody>) {
+                        if (response.isSuccessful) {
+
+                        } else {
+
+                        }
+                    }
+
+                    override fun onError(e: Throwable) {
+                        Toast.showToast(requireContext(), "网络请求失败: ${e.message}")
+                    }
+
+                    override fun onComplete() {}
+                })
+        }
     }
+
 
     override fun onStart() {
         super.onStart()
